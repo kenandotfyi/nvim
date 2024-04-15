5fb4be39fddd6816cbbc08947a5b6b30ab749d8c
kenandotfyi <kenan.akinci@icloud.com>
2024-04-15 23:40:22 +0200

    added border to noice lsp handlers

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -24,6 +24,7 @@ return {
     },
     presets = {
       bottom_search = false,
+      lsp_doc_border = true,
       command_palette = true,
       long_message_to_split = true,
       inc_rename = true,
